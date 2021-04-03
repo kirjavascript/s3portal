a4142b6f1af841dac64e82583321b46e767900a9
kirjavascript <snkenjoi@gmail.com>
2021-04-03 14:41:43 +0100

    &knuckles assets

---
+++ resources/andknuckles.asm
@@ -0,0 +1,10 @@
+Mappings:
+Mappings_0: 	dc.w Mappings_2-Mappings
+Mappings_2: 	dc.b $0, $6
+	dc.b $F4, $E, $0, $0, $FF, $AD
+	dc.b $F4, $E, $0, $C, $FF, $CD
+	dc.b $F4, $E, $0, $18, $FF, $ED
+	dc.b $F4, $E, $0, $24, $0, $D
+	dc.b $F4, $E, $0, $30, $0, $2D
+	dc.b $F4, $2, $0, $3C, $0, $4D
+	even
